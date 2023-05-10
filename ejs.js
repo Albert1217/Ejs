b946b82da64326c6c7696e690164c6302c58d201
Albert1217 <ismoiljonmuxtorov880@gmail.com>
2023-05-10 16:04:54 +0900

    app.post changed

--- ejs.js
+++ ejs.js
@@ -3,6 +3,16 @@ const express = require("express");
 const res = require("express/lib/response");
 const app = express();
 const http = require("http");
+const fs = require("fs");
+
+let user;
+fs.readFile("database/user.json", "utf8", (err, data) => {
+  if (err) {
+    console.log("ERROR", err);
+  } else {
+    user = JSON.parse(data);
+  }
+});
 
 // 1 Kirish code
 app.use(express.static("public"));
@@ -16,14 +26,15 @@ app.set("views", "views");
 app.set("view engine", "ejs");
 
 // 4 Routing code
-app.set("/create-item", (req, res) => {
-  console.log(req);
-  res.json({ tes: "success" });
+app.post("/create-item", (req, res) => {
+  //TODO: code with db here
+  //   console.log(req);
+  //   res.json({ tes: "success" });
 });
 
-// app.get("/author", (req, res) => {
-//   res.render("author");
-// });
+app.get("/author", (req, res) => {
+  res.render("author", { user: user });
+});
 
 app.get("/", function (req, res) {
   res.render("harid");
